8ee276b009040e91e0dafceee680884d22814b37
Zach Anderson <zach@zabot.dev>
2024-12-02 19:34:40 -0600

    Use an enum to encode signal ranges

--- src/syntax/sigspec.rs
+++ src/syntax/sigspec.rs
@@ -1,29 +1,40 @@
 use super::*;
 
 #[derive(Debug, Clone)]
+pub enum Index {
+    Offset(i64),
+    Range((i64, i64)),
+}
+
+impl fmt::Display for Index {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Index::Offset(offset) => write!(f, "[{}]", offset),
+            Index::Range((l, r)) => write!(f, "[{}:{}]", l, r),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub enum SigSpec {
-    Const((Const, i64, i64)),
-    Refer((String, i64, i64)),
+    Const((Const, Option<Index>)),
+    Refer((String, Option<Index>)),
     List(Vec<SigSpec>),
 }
 
 impl fmt::Display for SigSpec {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            SigSpec::Const((n, l, r)) => {
+            SigSpec::Const((n, index)) => {
                 write!(f, "{}", n)?;
-                if *r != 0 {
-                    write!(f, "[{}:{}]", l, r)?;
-                } else if *l != 0 {
-                    write!(f, "[{}]", l)?;
+                if let Some(index) = index {
+                    write!(f, "{}", index)?;
                 }
             }
-            SigSpec::Refer((n, l, r)) => {
+            SigSpec::Refer((n, index)) => {
                 write!(f, "{}", n)?;
-                if *r != 0 {
-                    write!(f, "[{}:{}]", l, r)?;
-                } else if *l != 0 {
-                    write!(f, "[{}]", l)?;
+                if let Some(index) = index {
+                    write!(f, "{}", index)?;
                 }
             }
             SigSpec::List(n) => {
